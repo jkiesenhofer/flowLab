c136ad258b137350584c7f5d428b96fede32d197
jkiesenhofer <jkxf489vyn@privaterelay.appleid.com>
2026-01-27 15:45:03 +0100

    Create reynolds.m

---
+++ reynolds.m
@@ -0,0 +1,28 @@
+clear; clc;
+
+% Parameter
+rho_p = 2500;
+rho_b = 1.293;
+mu    = 1.8e-5;
+Ub    = 5.0;
+L     = 0.1;
+
+i = 1:100;
+
+dp = i * 1e-6;     % Partikeldurchmesser [m]
+db = i * 1e-4;     % Blasendurchmesser [m]
+
+% Kennzahlen
+St  = rho_p .* dp.^2 .* Ub ./ (18 * mu * L);
+Reb = Ub .* db .* rho_b ./ mu;
+
+% Verhältnis
+ratio = St ./ Reb;
+
+% Plot
+figure
+plot(dp, ratio, 'LineWidth', 2)
+grid on
+xlabel('Partikeldurchmesser d_p [m]')
+ylabel('St / Re_b [-]')
+title('Verhältnis Stokeszahl zu Reynolds-Zahl')
