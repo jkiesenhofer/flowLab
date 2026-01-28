01c5b84413c01fdb37f42377d3b794784b379562
jkiesenhofer <jkxf489vyn@privaterelay.appleid.com>
2026-01-28 21:05:45 +0100

    Create bubble.m

---
+++ bubble.m
@@ -0,0 +1,29 @@
+theta = linspace(0,2*pi,200);
+
+r_base = 1;
+r_distort = 0.2*sin(3*theta) + 0.1*cos(5*theta);
+
+r = r_base + r_distort;
+
+x = r .* cos(theta);
+y = r .* sin(theta);
+
+figure
+fill(x,y,[0.15 0.7 0.5])
+axis equal
+
+subplot(1,2,1)
+fill(x,y,[0.15 0.7 0.5])
+axis equal
+xlim([-2 2])
+ylim([-2 2])
+
+x = r .* cos(theta)/2;
+y = r .* sin(theta)/2+1;
+
+subplot(1,2,2)
+fill(x,y,[0.15 0.7 0.5])
+axis equal
+
+xlim([-2 2])
+ylim([-2 2])
