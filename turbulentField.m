d0fb9005d8080c0e46c70d02de2f197d63d8bbaf
jkiesenhofer <jkxf489vyn@privaterelay.appleid.com>
2026-01-28 19:12:32 +0100

    Create turbulentField.m

---
+++ turbulentField.m
@@ -0,0 +1,24 @@
+[x,y] = meshgrid(linspace(0,2*pi,100));
+rho = 7900;
+mu = 1e-1;
+sigma = 0.072;
+L = sqrt(max(x).^2 + max(y).^2);
+
+u =  sin(y) + 0.5*cos(3*x).*sin(2*y);
+v = -sin(x) + 0.5*sin(3*y).*cos(2*x);
+
+
+speed = sqrt(u.^2 + v.^2);
+Re = rho * speed .* L / mu;
+
+figure
+imagesc(x(1,:), y(:,1), Re)
+set(gca,'YDir','normal')
+colorbar
+xlabel('x')
+ylabel('y')
+title('Re(x,y)')
+
+hold on
+quiver(x,y,u,v,'k')
+hold off
