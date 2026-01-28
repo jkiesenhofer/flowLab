f78070b6362347cad8b78aaa48b3f611f3ccb98a
jkiesenhofer <jkxf489vyn@privaterelay.appleid.com>
2026-01-28 18:30:33 +0100

    Update steo.m

--- steo.m
+++ steo.m
@@ -9,7 +9,7 @@ tau_f = 0.01;      % charakteristische Strömungszeit [s]
 
 % Partikeldurchmesser
 d = linspace(1e-5, 2e-3, 200);  % [m]
-
+v = randi(500,1,200);
 % Eötvöszahl
 Eo = (rho_l - rho_g) .* g .* d.^2 ./ sigma; % Gravitationskräfte zu Oberflächenspannungskräfte
 
@@ -19,7 +19,7 @@ St = tau_p ./ tau_f     % Partikelträgheit zur Strömungsgeschwindigkeit des Fl
 
 % Plot
 figure
-plot(St, Eo, 'LineWidth', 2)
+loglog(St, Eo, 'LineWidth', 2)
 xlabel('Stokeszahl St')
 ylabel('Eötvöszahl Eo')
 grid on
