a996ac528d84b48b652b89806f7408b4cbaac4d1
jkiesenhofer <jkxf489vyn@privaterelay.appleid.com>
2026-03-15 18:01:46 +0100

    Update wave.cpp

--- wave.cpp
+++ wave.cpp
@@ -7,13 +7,14 @@ int main() {
     const int width = 100;   // horizontal size of the graph
     const int height = 25;   // vertical size of the graph
     const double pi = 3.14159265358979323846;
-    std::cout << "#################################";
-    std::cout << "#################################";
-    std::cout << "#################################"<< std::endl;
+    //std::cout << "#################################";
+    //std::cout << "#################################";
+    //std::cout << "#################################"<< std::endl;
     // Find max and min for scaling
     double maxVal = -1e9, minVal = 1e9;
     std::list<int> myList = {1, 2, 3, 4, 5};
     int* ptr = nullptr;
+    static int value = 10;
     double width2[3] = {10.5, 20.75, 30.0};
     for (int x = 0; x < width; ++x) {
         double angle = (double)x / width * 10 * pi;  // choose scale to show enough cycles
@@ -39,8 +40,8 @@ int main() {
         std::cout << std::endl;
         
     }
-    std::cout << "#################################";
-    std::cout << "#################################";
-    std::cout << "#################################"<< std::endl;
+    //std::cout << "#################################";
+    //std::cout << "#################################";
+    //std::cout << "#################################"<< std::endl;
     return 0;
 }
