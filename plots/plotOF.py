a21d1074a2669296ef112be7f46400197b7a36d4
jkiesenhofer <jkxf489vyn@privaterelay.appleid.com>
2026-03-24 14:38:26 +0100

    Update plotOF.py

--- plots/plotOF.py
+++ plots/plotOF.py
@@ -1,6 +1,8 @@
-import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
+import math
+# https://doi.org/10.1016/S0301-7516(97)00073-2
+# Generate x values from 0 to 0.5 pi
 
 # Enable LaTeX-style fonts
 plt.rcParams.update({
@@ -13,39 +15,40 @@ plt.rcParams.update({
     "axes.titlesize": 16
 })
 
-# Load CSV file
-df = pd.read_csv("OF/flotation_10/data.csv")
+np.cot = lambda x: 1 / np.tan(x)
 
-# Compute mean
-mean_val = df["p_rgh"].mean()
-''''
-# Plot histogram
-plt.hist(df["p_rgh"].sort_values())
+phim = np.linspace(1e-6, 0.5 * np.pi, 500)  # 500 points for smooth curve
+phi0 = 10*np.pi/180
+Db = 0.001 
+Dp = 0.0001
+U = 4
 
-# Add mean line
-plt.axvline(mean_val, color='red', linestyle='dashed', linewidth=2, label=f'Mean = {mean_val:.2f}')
-'''
+Re = U*Dp/10**(-6)
 
-x = df["Cell ID"]/max(df["Cell ID"])
-y = df["U.water_Magnitude"]*1000
+V = 1/6*np.pi*Db**3
+X = 3/2+9*Re/(32+9.888*Re**0.694)
+Y = 3*Re/(8+1.736*Re**0.518)
+C = V/U*(Db/Dp)**2
+D = (math.sqrt((X+C)**2+3*Y**2)-(X+C))/(3*Y)
+M = -9/4-27*Re/64+0.2266*Re**1.1274
+N = -0.437*Re**1.0562
 
-coeffs = np.polyfit(x, y, deg=3)  # deg=1 für linear
-a, c, m, b = coeffs
-print(f"Lineare Funktion: y = {m:.2f}*x + {b:.2f}")
+A = V/U + Db/Db*X+(Dp/Db)**2*M
+B = Dp/Db*Y/A+(Dp/Db)**2*N/A
 
-# Fit-Linie für Plot erstellen
-y_fit = m * x + b + c * x**2 + a*x**3
-plt.scatter(x,y, marker='.')
-plt.plot(x, y_fit, color='black', label='Cubic interpolation function')
-# Lineares Fit: y = m*x + b
+ts = ((Dp + Db) / (2*U*(1-B**2)*A) * 
+np.log((np.tan(phim/2)/np.tan(phi0/2))*
+(1/(np.sin(phim)+B*np.cot(phim))/
+1/(np.sin(phi0)+B*np.cot(phi0)))**B))
 
-# Labels and title using LaTeX
-plt.xlabel(r"\textbf{$\tilde{x}$}")
-plt.ylabel(r"Re")
-#plt.title(r"\textbf{Hydrostatic pressure contribution}")
-
-# Rotate x-ticks for readability
-plt.xticks(rotation=0)
+# Plot
+plt.plot(ts, phim*90/np.pi, label='ts', color='blue')
+#plt.title('Bubble–particle collision and attachment')
+plt.xlabel('Approaching angle ')
+plt.ylabel('Induction time in s')
+plt.grid(True)
 plt.legend()
-plt.tight_layout()
+#plt.ylim(0, max(ts))
 plt.show()
+
+#plt.savefig("plot.png")
